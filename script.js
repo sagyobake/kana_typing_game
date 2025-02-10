9b93195c90afce117d8d23164f9864bc7a6e91ea
sagyobake <sagyobake@localhost.localdomain>
2025-02-10 17:11:13 +0900

    upgrade

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const question = document.getElementById('question'); //問題分を表示
 const answer = document.getElementById('answer'); //ユーザの解答を表示
 let i = 0; //ユーザーが正解していくごとにカウントしていく
 
-const kana = [...'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん'];
+const kana = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん'.split('');
 
 console.log(kana);
 
-const alphabet = '3 e 4 5 6 t g h @ b x d r p c q a z w s u i 1 , k f v 2 ^ - j n [ ] m 7 8 9 o l . ; \ 0 / y'.split(' ');
+const alphabet = '3 e 4 5 6 t g h @ b x d r p c q a z w s u i 1 , k f v 2 ^ - j n [ ] m 7 8 9 o l . ; \\ 0 / y'.split(' ');
 
 console.log(alphabet);
 
-kana.forEach(element => { //五十音のあいうえおをつくる
-    question.innerText = kana.join('');
-});
+for (let i = 0; i < kana.length; i++) {
+    const span = document.createElement('span');
+    span.innerText = kana[i];
+    span.setAttribute('id', `id${i}`);
+    question.appendChild(span);
+}
 
 const clear = () => {
     alert('clear!');
@@ -22,15 +25,10 @@ const clear = () => {
 
 //キーボードが押されたときの動作
 document.addEventListener('keydown', (e) => {
-    const index = alphabet.indexOf(e.key);
-    if (kana[index] !== undefined) {
-        console.log(kana[index]);
-        if (kana[index] === kana[i]) {
-            if (i === kana.kength - 1) { //全部打ち終わるとクリア表示する
-                clear();
-            }
-            i++;
-            answer.innerText += kana[index];
-        }
+
+    console.log(e.key);
+    if (e.key === alphabet[i]) {
+        document.getElementById(`id${i}`).style.color = 'orange';
+        i++;
     }
 });
